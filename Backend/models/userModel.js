747cb7e2a307f68059579e842dc764fe2b3a8477
Hrithik200 <64530006+Hrithik200@users.noreply.github.com>
2023-06-15 00:21:49 +0530

    login Complete

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-const userModel = mongoose.Schema(
+const bcrypt = require("bcryptjs");
+const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
@@ -25,6 +25,17 @@ const userModel = mongoose.Schema(
     timeStamps: true,
   }
 );
-const User = mongoose.model("Message", userModel);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+const User = mongoose.model("Message", userSchema);
 
 module.exports = User;
